178151a50cde662d4135ea96acd449ea28c38faf
orshamir1 <ororor38@gmail.com>
2023-03-02 14:44:20 +0200

    sperated server to server and routes file

--- server.js
+++ server.js
@@ -1,60 +1,14 @@
 const express = require('express');
-const axios = require('axios');
 const path = require('path');
+const api = require('./server/routes/recipesApi')
 app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname,'node_modules')));
+app.use('/', api)
 
-const findDairyIngredient = function (recipesArray) {
-    dairyIngredients = ["Cream","Cheese","Milk","Butter","Creme","Ricotta","Mozzarella","Custard","Cream Cheese"];
-    for(let dairy of dairyIngredients) {
-        for(let recipeIndex in recipesArray) {
-            let recipeIngredient = recipesArray[recipeIndex].ingredients
-            recipeIngredient.find(i => {
-                if(i == dairy) {
-                    recipesArray.splice(recipeIndex, 1);
-                }
-            })
-        }
-    }
-}
-
-const findGlutanIngredient = function (recipesArray) {
-    glutenIngredients = ["Flour","Bread","spaghetti","Biscuits","Beer"];
-    for(let glutan of glutenIngredients) {
-        for(let recipeIndex in recipesArray) {
-            let recipeIngredient = recipesArray[recipeIndex].ingredients
-            recipeIngredient.find(i => {
-                if(i == glutan) {
-                    recipesArray.splice(recipeIndex, 1);
-                }
-            })
-        }
-    }
-}
-app.get(`/recipes/:ingredient`, function (req, res) {
-    const filterParams = req.query;
-    const ingredient = req.params.ingredient
-    axios.get(`https://recipes-goodness-elevation.herokuapp.com/recipes/ingredient/${ingredient}?`)
-        .then((recipes) => {
-            if(!recipes.data.results.length) {
-                res.status(404).send({"Error" : `We don't have any avilable recepies with ${ingredient}`})
-                return
-            }
-            const recipesList = recipes.data.results;
-            if(filterParams.dairy == 'true') {
-                findDairyIngredient(recipesList)
-            }
-            if(filterParams.gluten == 'true') {
-                findGlutanIngredient(recipesList)
-            }
-            res.send(recipesList)  
-    })
-    
-})
 const port = 3000
 app.listen(port, function () {
     console.log(`Server running on ${port}`);
---
+++ server/routes/recipesApi.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const router = express.Router()
+const axios = require('axios');
+
+const findDairyIngredient = function (recipesArray) {
+    dairyIngredients = ["Cream","Cheese","Milk","Butter","Creme","Ricotta","Mozzarella","Custard","Cream Cheese"];
+    for(let dairy of dairyIngredients) {
+        for(let recipeIndex in recipesArray) {
+            let recipeIngredient = recipesArray[recipeIndex].ingredients
+            recipeIngredient.find(i => {
+                if(i == dairy) {
+                    recipesArray.splice(recipeIndex, 1);
+                }
+            })
+        }
+    }
+}
+
+const findGlutanIngredient = function (recipesArray) {
+    glutenIngredients = ["Flour","Bread","spaghetti","Biscuits","Beer"];
+    for(let glutan of glutenIngredients) {
+        for(let recipeIndex in recipesArray) {
+            let recipeIngredient = recipesArray[recipeIndex].ingredients
+            recipeIngredient.find(i => {
+                if(i == glutan) {
+                    recipesArray.splice(recipeIndex, 1);
+                }
+            })
+        }
+    }
+}
+router.get(`/recipes/:ingredient`, function (req, res) {
+    const filterParams = req.query;
+    const ingredient = req.params.ingredient
+    axios.get(`https://recipes-goodness-elevation.herokuapp.com/recipes/ingredient/${ingredient}?`)
+        .then((recipes) => {
+            if(!recipes.data.results.length) {
+                res.status(404).send({"Error" : `We don't have any avilable recepies with ${ingredient}`})
+                return
+            }
+            const recipesList = recipes.data.results;
+            if(filterParams.dairy == 'true') {
+                findDairyIngredient(recipesList)
+            }
+            if(filterParams.gluten == 'true') {
+                findGlutanIngredient(recipesList)
+            }
+            res.send(recipesList)  
+    })
+    
+})
+
+module.exports = router
