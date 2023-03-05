fa0a8183183d7c061b0ed300649e623d25c1cba9
orshamir1 <ororor38@gmail.com>
2023-03-05 10:56:12 +0200

    pages option with css

--- dist/main.js
+++ dist/main.js
@@ -1,29 +1,26 @@
 const renderer = new Renderer()
-const getRecipes = function() {
+let currentrecipesData = []
+const getRecipes = function () {
     const dairyCheckBox = $("#dairy-checkbox")[0].checked
     const glutenCheckBox = $("#gluten-checkbox")[0].checked
     const WantedIngredient = $("input").val()
-    return $.get(`recipes/${WantedIngredient}?dairy=${dairyCheckBox}&gluten=${glutenCheckBox}`)       
+    return $.get(`recipes/${WantedIngredient}?dairy=${dairyCheckBox}&gluten=${glutenCheckBox}`)
 }
-const createNewPages = function (recpicesData) {
-    const numberOfPagesNeeded = (recpicesData.length / 5)
-    const pagesNameArray = []
-    for(let i = 1; i < numberOfPagesNeeded; i++) {
-        pagesNameArray.push(i);
+const numerOfNeededPages = function (recpicesData) {
+    const pageAmount = (recpicesData.length / 5)
+    const pagesNamesArray = []
+    for (let i = 1; i < pageAmount; i++) {
+        pagesNamesArray.push(i);
     }
-    return pagesNameArray;
+    return pagesNamesArray;
 }
 
-const renderNewPage = function(recpicesData, pageNum) {
+const renderNewPage = function (recpicesData, pageNum) {
     $("#recipes-container").empty()
-    let TopindexToRender = 0;
-    if(pageNum > 0) {
-        TopindexToRender = pageNum * 5;
-    }
-    else if(pageNum == 0) {
-        TopindexToRender = 5; 
-    }
-    for(let i = (TopindexToRender - 5); i < TopindexToRender; i++) {
+    const numberOfItemsInOnePage = 5;
+    let topindexToRender = pageNum * numberOfItemsInOnePage;
+    const lowIndexToRender = (topindexToRender - numberOfItemsInOnePage);
+    for (let i = lowIndexToRender; i < topindexToRender; i++) {
         const title = recpicesData[i].title
         const picture = recpicesData[i].thumbnail
         const idMeal = recpicesData[i].idMeal
@@ -34,14 +31,15 @@ const renderNewPage = function(recpicesData, pageNum) {
         renderer.renderRecipe(title, picture, embedUrl, ingredients, idMeal)
     }
 }
-let currentrecipesData = []
-$("#submit").on('click', function (){
+
+$("#submit").on('click', function () {
     getRecipes().then(recpicesData => {
         currentrecipesData = recpicesData;
+        $("#pages-container").empty()
         $("#user-input").val("")
-        const numberOfpages = createNewPages(recpicesData)
+        const numberOfPages = numerOfNeededPages(recpicesData)
         renderNewPage(recpicesData, 1)
-        renderer.renderPageCounter(numberOfpages)
+        renderer.renderPageCounter(numberOfPages)
     }).catch(err => {
         const serverErrorMassage = err.responseJSON.Error
         alert(serverErrorMassage)
@@ -49,7 +47,7 @@ $("#submit").on('click', function (){
 })
 
 $("body").on('click', '.play-button-outer', function () {
-    const videoId =  $(this).data().video
+    const videoId = $(this).data().video
     $(`#${videoId}`).addClass("active")
 })
 $("body").on('click', '.close-button', function () {
@@ -59,5 +57,4 @@ $("body").on('click', '.close-button', function () {
 $("body").on('click', ".page-number", function () {
     const pageNumber = $(this).attr("id");
     renderNewPage(currentrecipesData, pageNumber)
-
 })
